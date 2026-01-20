9e9c5026637d267af684bab398e61a8313ba34e0
davidegui <dseguiguren@gmail.com>
2026-01-20 01:57:15 +0000

    wip: first commands

--- semana3/script_s3.sh
+++ semana3/script_s3.sh
@@ -1,3 +1,10 @@
 ls -l /home  
 touch 'script s3'
 cat script script\ s3
+echo Hola Unix
+printf 'Hola UNIX\n'
+echo Hola UNIX1
+echo Hola UNIX2
+printf 'Hola %s\n' 'Hola UNIX'
+ps -a -u -x
+ps -aux

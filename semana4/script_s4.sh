14afb64eda3f832e18218d7f8e7ba0d71b1c35ed
Jonathan Tito <jotitoon@uide.edu.ec>
2026-01-27 01:14:38 +0000

    feat: add initial files and script for semana4

---
+++ semana4/script_s4.sh
@@ -0,0 +1,10 @@
+mkdir -p padre_jt/hijo_jt
+pwd
+cd
+pwd
+cd /workspaces/UNIX_2-SIN-2A_2026_ene_mar/semana4/padre_jt/hijo_jt
+touch archivo_jt.txt
+echo '0' > archivo_jt.txt
+echo '1' >> archivo_jt.txt
+echo '2' >> archivo_jt.txt
+echo '3' >> archivo_jt.txt

a6700e0b8afd9dc38c56422073de3ca470403a51
davidegui <dseguiguren@gmail.com>
2026-01-29 21:44:21 +0000

    wip

--- semana4/script_s4.sh
+++ semana4/script_s4.sh
@@ -2,8 +2,9 @@ mkdir -p padre_de/hijo_de
 pwd 
 cd 
 pwd
-cd /workspaces/UNIX_2-SIN-2A_2026_ene_mar/padre_de/hijo_de
+cd /workspaces/UNIX_2-SIN-2A_2026_ene_mar/semana4/padre_de/hijo_de
 touch archivo_de.txt
+echo '0' > archivo.txt
 echo '1' >> archivo_de.txt
 echo '2' >> archivo_de.txt
-echo '3' >> archivo_de.txtSSS
+echo '3' >> archivo_de.txt

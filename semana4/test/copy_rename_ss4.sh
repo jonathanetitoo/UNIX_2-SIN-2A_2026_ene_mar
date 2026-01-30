b617da0a65725f73afe2ecde99036a182049b071
davidegui <dseguiguren@gmail.com>
2026-01-30 23:10:17 +0000

    wip

--- semana4/test/copy_rename_ss4.sh
+++ semana4/test/copy_rename_ss4.sh
@@ -4,7 +4,7 @@ cd
 pwd
 cd /workspaces/UNIX_2-SIN-2A_2026_ene_mar/semana4/padre_de/hijo_de
 touch archivo_de.txt
-echo '0' > archivo.txt
+echo '0' > archivo_de.txt
 echo '1' >> archivo_de.txt
 echo '2' >> archivo_de.txt
 echo '3' >> archivo_de.txt
